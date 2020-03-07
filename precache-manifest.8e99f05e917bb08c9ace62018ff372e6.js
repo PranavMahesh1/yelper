788a3e357d6a0bd940b0fb92d7aee0c2b980272b
Pranav Mahesh <pmahesh@gatech.edu>
2020-03-06 21:46:02 -0500

    Updates

--- precache-manifest.8e99f05e917bb08c9ace62018ff372e6.js
+++ precache-manifest.8e99f05e917bb08c9ace62018ff372e6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d886aac8daba4fe5d4ba52c1e119a694",
+    "revision": "19fbbfc93ec46c530803a1d73aa81968",
     "url": "/restaurant-searcher/index.html"
   },
   {
-    "revision": "bc9d8ab582f4bedc1adf",
+    "revision": "963313f76684d6b8a13b",
     "url": "/restaurant-searcher/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "a83bc320b9142f39d1ab",
+    "revision": "d48a7ef57cfa6b90e2be",
     "url": "/restaurant-searcher/static/css/main.7ab682bd.chunk.css"
   },
   {
-    "revision": "bc9d8ab582f4bedc1adf",
-    "url": "/restaurant-searcher/static/js/2.337a6a17.chunk.js"
+    "revision": "963313f76684d6b8a13b",
+    "url": "/restaurant-searcher/static/js/2.991015ec.chunk.js"
   },
   {
     "revision": "5f2dfc729cbc5e03eb64677a2da4dad4",
-    "url": "/restaurant-searcher/static/js/2.337a6a17.chunk.js.LICENSE.txt"
+    "url": "/restaurant-searcher/static/js/2.991015ec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a83bc320b9142f39d1ab",
-    "url": "/restaurant-searcher/static/js/main.fdaf01fe.chunk.js"
+    "revision": "d48a7ef57cfa6b90e2be",
+    "url": "/restaurant-searcher/static/js/main.15f44da6.chunk.js"
   },
   {
     "revision": "9f8aefb029ea59f388fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/restaurant-searcher/precache-manifest.abd43bc1583b30ecb0cfeb5c08e42334.js"
+  "/restaurant-searcher/precache-manifest.8e99f05e917bb08c9ace62018ff372e6.js"
 );
 
 self.addEventListener('message', (event) => {
